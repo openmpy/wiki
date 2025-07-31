3d3f34e4a779c056c41f483f46e2edee4b04132b
openmpy <suhwan@kakao.com>
2025-07-31 20:42:08 +0900

    feat: 초기 설정

---
+++ service/dictionary/src/main/java/openmpy/taleswiki/dictionary/DictionaryApplication.java
@@ -0,0 +1,12 @@
+package openmpy.taleswiki.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(DictionaryApplication.class, args);
+    }
+}
